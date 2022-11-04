25b97b7ffa76e3efd524c5663f25ea6119974f5c
GamerBike39 <champidub@gmail.com>
2022-11-04 13:52:21 +0100

    voyager

--- laravel/app/Models/Livre.php
+++ laravel/app/Models/Livre.php
@@ -4,9 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Livre extends Model
 {
     use HasFactory;
-    protected $fillable = ['title', 'author', 'desc', 'price'];
+    protected $fillable = ['title', 'author', 'desc', 'price','image'];
+
+    public function getSlugAttribute(): string
+    {
+        return Str::slug($this->title);
+    }
+
+
 }
